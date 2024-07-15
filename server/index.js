f806a49a7e9a2887a5254b5515fdcad40a4d3045
Pallavi-Pandey <pallavipandey181@gmail.co>
2024-07-15 21:14:11 +0530

    1:16:38

--- server/index.js
+++ server/index.js
@@ -3,12 +3,17 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async(req, res) => {
     res.send('Hello from OpenAI!');
@@ -17,11 +22,10 @@ app.get('/', async(req, res) => {
 const startServer = async () => {
     try {
       connectDB(process.env.MONGODB_URL);
-      app.listen(8080, () => console.log('Server started on port 8080'));
+      app.listen(8080, () => console.log('Server started on port http://localhost:8080'));
     } catch (error) {
       console.log(error);
     }
   };
 
 startServer();
-// mongodb+srv://pallavi:pallavi@cluster0.tj8oxen.mongodb.net/
